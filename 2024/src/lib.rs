e41f47d5c20cb605c97b13d3bb3c5591a472d1cc
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-11-29 15:59:34 -0500

    trait for solution

--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::{Debug, Display};
+
+pub trait Solution {
+    type Parsed: Debug + Clone;
+    type Answer: Debug + Display + PartialEq;
+    const INPUT: &'static str;
+    const SAMPLE_INPUT: &'static str;
+    const SAMPLE_ANSWER_A: &'static str;
+    const SAMPLE_ANSWER_B: &'static str;
+
+    fn parse(input: &str) -> Self::Parsed;
+    fn part_a(input: Self::Parsed) -> Self::Answer;
+    fn part_b(input: Self::Parsed) -> Self::Answer;
 }
